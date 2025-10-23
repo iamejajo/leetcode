bf062d57866202d534c0e8887aecae50e4254794
alazar <ej.dalvic@gmail.com>
2025-10-23 21:27:17 +0300

    Time: 11 ms (36.12%), Space: 17.7 MB (89.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+    #    using string solution
+    #    stringx = str(x)
+    #    return stringx == stringx[::-1]
+
+       forward = x
+       reverse = 0
+       if x == 0:
+        True #0 is always a palindrom
+
+       while x > 0:
+        lastdigit = x%10
+        reverse = reverse * 10 + lastdigit
+        x = x//10
+
+       return forward == reverse
+       
+        
